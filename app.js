a65b36a647b01cf09baf03bc3fc61186df1539b9
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-03-18 16:08:47 +0100

    add:cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -10,7 +11,7 @@ const tagRouter = require('./routes/tagRouter');
 const userRouter = require('./routes/userRouter');
 
 const app = express();
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
