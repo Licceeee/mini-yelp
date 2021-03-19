854c61961a75cdfe7b4558baa7df1dec9af4cde6
Fred Miller <fmdiving001@gmail.com>
2021-03-19 13:18:14 +0100

    addedauth

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const jwt = require('jsonwebtoken')
 const restaurantRouter = require('./routes/restaurantRouter');
 const cityRouter = require('./routes/cityRouter');
 const tagRouter = require('./routes/tagRouter');
 const userRouter = require('./routes/userRouter');
 const reviewRouter = require('./routes/reviewRouter');
-
+const authRouter = require('./routes/authenticationRouter')
 const app = express();
 app.use(cors());
 app.use(logger('dev'));
@@ -24,5 +24,5 @@ app.use('/api/v1/cities', cityRouter);
 app.use('/api/v1/tags', tagRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
-
+app.use('/login',authRouter),
 module.exports = app;
